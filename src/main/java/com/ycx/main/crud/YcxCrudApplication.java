379a52305bc62e01702d872dff3ff92e5abf1728
xq <337287420@qq.com>
2023-05-23 10:30:05 +0800

    init

---
+++ src/main/java/com/ycx/main/crud/YcxCrudApplication.java
@@ -0,0 +1,12 @@
+package com.ycx.main.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YcxCrudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YcxCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ycx/main/crud/YcxCrudApplicationTests.java
@@ -0,0 +1,18 @@
+package com.ycx.main.crud;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+class YcxCrudApplicationTests {
+
+    @Test
+    void test() {
+    }
+
+}
